b7e3434b3eefaea131845bd288702403e29104d1
anindya pal <anindya2674@gmail.com>
2016-07-03 20:55:49 +0530

    New setup file with shortcut creation

--- Source codes/setup.py
+++ Source codes/setup.py
@@ -9,7 +9,8 @@ if sys.platform == 'win32':
     ba = "Win32GUI"
 
 
-execu = [Executable(script = "nita_di_news.py", base = ba, copyDependentFiles=True,
+execu = [Executable(script = "nita_di_news.py",shortcutName="nita_di_news",
+            shortcutDir="DesktopFolder", base = ba, copyDependentFiles=True,
     appendScriptToExe=True,
     appendScriptToLibrary=False, targetName = "nita_di_news.exe")]
 
@@ -17,7 +18,7 @@ execu = [Executable(script = "nita_di_news.py", base = ba, copyDependentFiles=Tr
     
 setup(
     name="NITA NEWS",
-    options = {"build_exe": {"packages":["os"],"include_files":["nita_icon.ico"]}},
+    options = {"build_exe": {"packages":["os"],"include_files":["..////Resources//nita_icon.ico"]}},
     version = "1.1",
     description = "View Latest News currently at NITA website",
     executables = execu)
